willthefrog/models

Fix eval during training
looks like a regression introduced by frequent rebasing

PaddleCV/PaddleDetection/tools/train.py
@@ -241,7 +241,7 @@ def main():
             if FLAGS.eval:
                 anno_file = getattr(eval_loader.dataset, 'annotation_file', None)
                 # evaluation
-                results = eval_run(exe, compiled_eval_prog, eval_pyreader,
+                results = eval_run(exe, compiled_eval_program, eval_loader,
                                    eval_keys, eval_values, eval_cls)
                 resolution = None
                 if 'mask' in results[0]:

willthefrog/PaddleDetection

tools/train.py
@@ -250,7 +250,7 @@ def main():
             if FLAGS.eval:
                 anno_file = getattr(eval_loader.dataset, 'annotation_file', None)
                 # evaluation
-                results = eval_run(exe, compiled_eval_prog, eval_pyreader,
+                results = eval_run(exe, compiled_eval_prog, eval_loader,
                                    eval_keys, eval_values, eval_cls)
                 resolution = None
                 if 'mask' in results[0]: